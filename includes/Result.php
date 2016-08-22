822f4ad2197245748bf4736ddf16d3d142cabb4f
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-08-23 00:59:28 +0900

    解答の結果に応じてOGPの画像を変更

--- includes/Result.php
+++ includes/Result.php
@@ -18,43 +18,82 @@ class Result {
 	 */
 	public function __construct() {
 
-		add_action( 'query_vars',  [ $this, 'add_point_query_var'] );
-		add_filter( 'the_content', [ $this, 'the_content'] );
-		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts'] );
+		add_action( 'query_vars', [ $this, 'add_point_query_var' ] );
+		add_filter( 'the_content', [ $this, 'the_content' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+		add_filter( 'jetpack_images_get_images', [ $this, 'jetpack_images_get_images' ], 10, 3 );
 	}
 
 	public function add_point_query_var( $vars ) {
 		$vars[] = 'point';
 		$vars[] = 'embedid';
+
 		return $vars;
 	}
 
 	private function get_point() {
 		$point = get_query_var( 'point', 0 );
+
 		return intval( $point );
 	}
 
 	/**
 	 * @param $post_id
+	 * @param bool $html
 	 *
-	 * @return array
+	 * @return array|string
 	 */
-	private function get_result_image( $post_id ) {
-		$filtered = array_filter(  $this->get_images( $post_id ), function( $image ) {
+	private function get_result_image( $post_id , $html = true, $size = 'full' ) {
+		$filtered = array_filter( $this->get_images( $post_id ), function ( $image ) {
 			return ( $image['threshold'] <= $this->get_point() );
-		});
+		} );
 
 		$result = end( $filtered );
-		if( $result ) {
-			return wp_get_attachment_image( $result['image_id'], 'full' );
+		if ( $result ) {
+			if( $html ) {
+				return wp_get_attachment_image( $result['image_id'], $size );
+			}
+			else {
+				return wp_get_attachment_image_src(  $result['image_id'], $size );
+			}
 		}
 
 		return '';
 
 	}
 
-	private function get_app_page_link( ) {
+	/**
+	 * @param array $media Array of images that would be good for a specific post.
+	 * @param int $post_id Post ID.
+	 * @param array $args Array of options to get images.
+	 *
+	 * @return array
+	 */
+	public function jetpack_images_get_images( $media, $post_id, $args = [] ) {
+
+		$post = get_post( $post_id );
+
+		if ( 'quiz' != get_post_type( $post ) ) {
+			return $media;
+		}
+
+		$result_image = $this->get_result_image( $post_id , false );
+
+		$media = array(
+			'type'       => 'image',
+			'from'       => 'thumbnail',
+			'src'        => $result_image[0],
+			'src_width'  => $result_image[1],
+			'src_height' => $result_image[2],
+			'href'       => get_permalink($result_image[0])
+		);
+
+		return array($media);
+	}
+
+	private function get_app_page_link() {
 		$embedid = get_query_var( 'embedid', 0 );
+
 		return $link = get_permalink( intval( $embedid ) );
 	}
 
@@ -66,13 +105,13 @@ class Result {
 
 	public function the_content( $html ) {
 		$post = get_post();
-		if( 'quiz' != get_post_type( $post ) ) {
+		if ( 'quiz' != get_post_type( $post ) ) {
 			return $html;
 		}
 
 		$question_link = '';
-		if( $href = $this->get_app_page_link() ) {
-			$question_link = sprintf('<p><a href="%s" class="btn" style="display: block">チャレンジ！</a></p>', $href);
+		if ( $href = $this->get_app_page_link() ) {
+			$question_link = sprintf( '<p><a href="%s" class="btn" style="display: block">チャレンジ！</a></p>', $href );
 		}
 
 		return sprintf(
@@ -100,7 +139,7 @@ class Result {
 			$this->get_share_link()
 		);
 
-		$facebook = '<div class="fb-share-button" data-href="'.$this->get_share_link().'" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">シェア</a></div>';
+		$facebook = '<div class="fb-share-button" data-href="' . $this->get_share_link() . '" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">シェア</a></div>';
 
 		$twitter = sprintf(
 			'<a href="https://twitter.com/share" class="twitter-share-button" data-text="%s" data-url="%s">Tweet</a>',
